8637080e59019293880ac6ad438c2011f1694b97
mamathak412 <mamathak412@gmail.com>
2020-07-22 01:56:18 -0400

    fixes before execution

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -4,5 +4,9 @@ echo "**********************"
 echo "**Building jar********"
 echo "**********************"
 
-docker run --rm -v $PWD/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
+docker cp jenkins:/var/jenkins_home /home/manisha/jenkins
+
+WORKSPACE=/home/manisha/jenkins/jenkins_home/workspace/PipelineCICD
+
+docker run --rm -v $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
 
--- jenkins/test/mvn.sh
+++ jenkins/test/mvn.sh
@@ -4,5 +4,7 @@ echo "**********************"
 echo "***Testing the jar********"
 echo "**********************"
 
-docker run --rm -v $PWD/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
+WORKSPACE=/home/manisha/jenkins/jenkins_home/workspace/PipelineCICD
+
+docker run --rm -v $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
 
