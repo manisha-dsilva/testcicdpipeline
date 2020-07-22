e91dbe3fb009fb7a44541d54bb5834a9504fb71f
mamathak412 <mamathak412@gmail.com>
2020-07-22 10:07:07 -0400

    changes

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -4,9 +4,9 @@ echo "**********************"
 echo "**Building jar********"
 echo "**********************"
 
-docker cp jenkins:/var/jenkins_home/workspace/demopipeline ../jenkins_home/workspace
+docker cp jenkins:/var/jenkins_home/workspace/PipelineCICD ../jenkins_home/workspace
 
-WORKSPACE=/home/manisha/jenkins/jenkins_home/workspace/demopipeline
+WORKSPACE=/home/manisha/jenkins/jenkins_home/workspace/PipelineCICD
 
 docker run --rm -v $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
 
--- jenkins/test/mvn.sh
+++ jenkins/test/mvn.sh
@@ -4,7 +4,7 @@ echo "**********************"
 echo "***Testing the jar********"
 echo "**********************"
 
-WORKSPACE=/home/manisha/jenkins/jenkins_home/workspace/demopipeline
+WORKSPACE=/home/manisha/jenkins/jenkins_home/workspace/PipelineCICD
 
 docker run --rm -v $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
 
