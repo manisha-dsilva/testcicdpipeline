e09fc4686fe23eb85c07eb0b891741dd8ef03137
mamathak412 <mamathak412@gmail.com>
2020-07-22 03:43:29 -0400

    changes

--- jenkins/build/build.sh
+++ jenkins/build/build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 #copy the new jar to build location
-cp -f ../../java-app/target/*.jar jenkins/build/
+cp -f ../../java-app/target/*.jar .
 
 echo "***************************"
 echo "***Building docker image***"
 echo "***************************"
 
-cd jenkins/build/ && docker-compose -f docker-compose-build.yml build --no-cache
+docker-compose --file docker-compose-build.yml build --no-cache
 
 
