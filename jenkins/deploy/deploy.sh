e9d282d5be6926b284f96f1ec8b2071190af4aa8
mamathak412 <mamathak412@gmail.com>
2020-07-23 06:29:41 -0400

    changes

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-echo docker_repo > /tmp/.auth
-echo $BUILD_TAG >> /tmp/.auth
-echo $PASS >> /tmp/.auth
+#echo docker_repo > /tmp/.auth
+#echo $BUILD_TAG >> /tmp/.auth
+#echo $PASS >> /tmp/.auth
 
 echo "****Deploying*******"
 
+docker logs deployed
