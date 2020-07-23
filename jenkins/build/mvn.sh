f51c65e1c183d679d643ce22ba9c999515743fe8
mamathak412 <mamathak412@gmail.com>
2020-07-23 06:44:33 -0400

    change

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -4,7 +4,9 @@ echo "**********************"
 echo "**Building jar********"
 echo "**********************"
 
+docker cp jenkins:/var/jenkins_home/workspace/PipelineCICD ../jenkins_home/workspace
 
+cd ../jenkins_home/workspace && rm -rf .git
 
 WORKSPACE=/home/manisha/jenkins/jenkins_home/workspace/PipelineCICD
 
