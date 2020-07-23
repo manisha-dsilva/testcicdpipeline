25695775a58d090c5c22c6aae54405d66890dc38
mamathak412 <mamathak412@gmail.com>
2020-07-23 06:46:48 -0400

    changes

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -4,10 +4,6 @@ echo "**********************"
 echo "**Building jar********"
 echo "**********************"
 
-docker cp jenkins:/var/jenkins_home/workspace/PipelineCICD ../jenkins_home/workspace
-
-cd ../jenkins_home/workspace && rm -rf .git
-
 WORKSPACE=/home/manisha/jenkins/jenkins_home/workspace/PipelineCICD
 
 docker run --rm -v $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
