cedf8decdc5478e1c485e7264fc8cf56b18304e7
mamathak412 <mamathak412@gmail.com>
2020-07-23 05:25:08 -0400

    changes

--- jenkins/push/push.sh
+++ jenkins/push/push.sh
@@ -4,8 +4,8 @@ echo "****************************"
 echo "***Pushing image************"
 echo "****************************"
 
-IMAGE="docker_repo"
-BUILD_TAG=$(sed -n '2p' /tmp/.auth)
+#IMAGE="docker_repo"
+#BUILD_TAG=$(sed -n '2p' /tmp/.auth)
 
 echo "****login********"
 docker login -u manishadsilva -p $PASS
