35c5b88efd45ac2cb87318fc4818b7e921595be5
root <root@localhost.localdomain>
2020-08-10 10:46:46 -0400

    pipeline

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -4,6 +4,9 @@ echo "**********************"
 echo "**Building jar********"
 echo "**********************"
 
+chown 1000 /var/run/docker.sock
+docker cp jenkins:/var/jenkins_home/workspace/$PROJECT ../jenkins_home/workspace
+
 WORKSPACE=/home/manisha/jenkins/jenkins_home/workspace/$PROJECT
 
 docker run --rm -v $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
