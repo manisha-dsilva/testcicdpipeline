5a37c3430c70dc1e98ec948c76d9d066b6ec8e5f
root <root@localhost.localdomain>
2020-08-11 01:04:51 -0400

    pipeline

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -4,11 +4,15 @@ echo "**********************"
 echo "**Building jar********"
 echo "**********************"
 
-sudo chown 1000:1000 /var/lib/docker/volumes/jenkins-data -R
+sudo cp /var/lib/docker/volumes/jenkins-data/_data/workspace/$PROJECT ../jenkins_home/workspace
+
+sudo chown 1000:1000 jenkins-home -R
 
 sudo chown 1000 /var/run/docker.sock
 
-WORKSPACE=/var/lib/docker/volumes/jenkins-data/_data/workspace/$PROJECT
+#WORKSPACE=/var/lib/docker/volumes/jenkins-data/_data/workspace/$PROJECT
+
+WORKSPACE=/home/manisha/jenkins/jenkins_home/workspace/$PROJECT
 
 docker run --rm -v $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
 
--- jenkins/test/mvn.sh
+++ jenkins/test/mvn.sh
@@ -4,7 +4,8 @@ echo "**********************"
 echo "***Testing the jar********"
 echo "**********************"
 
-WORKSPACE=/var/lib/docker/volumes/jenkins-data/_data/workspace/$PROJECT
+#WORKSPACE=/var/lib/docker/volumes/jenkins-data/_data/workspace/$PROJECT
+WORKSPACE=/home/manisha/jenkins/jenkins_home/workspace/$PROJECT
 
 docker run --rm -v $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
 
